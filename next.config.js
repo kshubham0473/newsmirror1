cadb791ccd39acdd9ded1cab25f79d1140e1e2b6
kshubham0473 <kshubham0473@gmail.com>
2026-03-23 13:18:46 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest\.json$/],
 });
 
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
